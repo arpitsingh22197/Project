029b2260f38128455b8b9fb8216c0644f5d3a592
arpit singh <arpitsingh221972@gmail.com>
2026-06-29 14:38:52 +0530

    fix: allow null templateData in useWebContainer prop type

--- modules/webcontainers/hooks/useWebContainer.ts
+++ modules/webcontainers/hooks/useWebContainer.ts
@@ -5,7 +5,7 @@ import { WebContainer } from "@webcontainer/api";
 import { TemplateFolder } from "@/modules/playground/lib/path-to-json";
 
 interface UseWebContainerProps {
-  templateData: TemplateFolder;
+  templateData: TemplateFolder | null;
 }
 
 interface UseWebContaierReturn {
